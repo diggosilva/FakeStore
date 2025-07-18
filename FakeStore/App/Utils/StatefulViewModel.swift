c58224589d7ae4ee5effbae91f56a878bba0ac78
Diggo Silva <rdiggosilva@gmail.com>
2025-07-18 10:39:51 -0300

    Created StatefulViewModel protocol

--- FakeStore/App/Utils/StatefulViewModel.swift
+++ FakeStore/App/Utils/StatefulViewModel.swift
@@ -1,3 +1,12 @@
+//
+//  StatefulViewModel.swift
+//  FakeStore
+//
+//  Created by Diggo Silva on 17/07/25.
+//
+
+import Combine
+
 protocol StatefulViewModel {
     associatedtype State
     var statePublisher: AnyPublisher<State, Never> { get }
