b31f7101ff4d88fc6443a620f8a3d007829ef7b6
Diggo Silva <rdiggosilva@gmail.com>
2025-07-19 12:41:00 -0300

    Created CustomImageView

--- FakeStore/App/Components/ImageViews/CustomImageView.swift
+++ FakeStore/App/Components/ImageViews/CustomImageView.swift
@@ -5,4 +5,29 @@
 //  Created by Diggo Silva on 18/07/25.
 //
 
-import Foundation
+import UIKit
+
+final class CustomImageView: UIImageView {
+    
+    init(
+        cornerRadius: CGFloat = 8,
+        contentMode: UIView.ContentMode = .scaleAspectFit,
+        clipsToBounds: Bool = true,
+        hasBorder: Bool = false,
+        borderColor: UIColor = .lightGray,
+        borderWidth: CGFloat = 1
+    ) {
+        super.init(frame: .zero)
+        self.translatesAutoresizingMaskIntoConstraints = false
+        self.layer.cornerRadius = cornerRadius
+        self.contentMode = contentMode
+        self.clipsToBounds = clipsToBounds
+        
+        if hasBorder {
+            self.layer.borderColor = borderColor.cgColor
+            self.layer.borderWidth = borderWidth
+        }
+    }
+    
+    required init?(coder: NSCoder) { fatalError("init(coder:) has not been implemented") }
+}
