267f093db1472460dd254090d872e6bd13ba2c88
Diggo Silva <rdiggosilva@gmail.com>
2025-07-20 12:49:09 -0300

    FeedViewModel refactored

--- FakeStore/App/Screens/1-Feed/FeedViewModel.swift
+++ FakeStore/App/Screens/1-Feed/FeedViewModel.swift
@@ -15,51 +15,46 @@ enum FeedViewControllerStates {
 }
 
 protocol FeedViewModelProtocol: StatefulViewModel where State == FeedViewControllerStates {
-    func numbersOfSection() -> Int
-    func numberOfRowsInSection( numberOfRowsInSection section: Int) -> Int
+    func numberOfProducts() -> Int
     func cellForRowAt(indexPath: IndexPath) -> Product
-    func tableView(titleForHeaderInSection section: Int) -> String?
-    func loadDataProducts()
+    func fetchProducts()
 }
 
 class FeedViewModel: FeedViewModelProtocol {
     
     private let service = Service()
     private var sectionList: [Section] = []
+    private var products: [Product] = []
     
     @Published private var state: FeedViewControllerStates = .loading
+    @Published private(set) var filteredProducts: [Product] = []
     
     var statePublisher: AnyPublisher<FeedViewControllerStates, Never> {
         $state.eraseToAnyPublisher()
     }
     
-    func numbersOfSection() -> Int {
-        sectionList.count
-    }
-    
-    func numberOfRowsInSection( numberOfRowsInSection section: Int) -> Int {
-        return sectionList[section].list.count
+    func numberOfProducts() -> Int {
+        return filteredProducts.count
     }
     
     func cellForRowAt(indexPath: IndexPath) -> Product {
-        let section = sectionList[indexPath.section]
-        let product = section.list[indexPath.row]
-        return product
+        return filteredProducts[indexPath.row]
     }
     
-    func tableView(titleForHeaderInSection section: Int) -> String? {
-        return "\(sectionList[section].categoryProduct)".capitalized
+    func filterProducts(for category: String?) {
+        if let category = category {
+            filteredProducts = products.filter { $0.category.lowercased() == category.lowercased() }
+        } else {
+            filteredProducts = products
+        }
     }
     
-    func loadDataProducts() {
+    func fetchProducts() {
         service.getProducts { produtos in
-            let sectionElectronics = Section(categoryProduct: .electronics, list: produtos)
-            let sectionJewelery = Section(categoryProduct: .jewelery, list: produtos)
-            let sectionMenSClothing = Section(categoryProduct: .menSClothing, list: produtos)
-            let sectionWomenSClothing = Section(categoryProduct: .womenSClothing, list: produtos)
-            
-            self.sectionList.append(contentsOf: [sectionElectronics, sectionJewelery, sectionMenSClothing, sectionWomenSClothing])
+            self.products.append(contentsOf: produtos)
+            self.filteredProducts = self.products
             self.state = .loaded
+            
         } onError: { error in
             self.state = .error
         }
