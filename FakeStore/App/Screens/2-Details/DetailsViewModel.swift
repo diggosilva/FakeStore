e542d26b61f5c0273dbd5e80ff12e2392f8902cb
Diggo Silva <rdiggosilva@gmail.com>
2025-07-22 14:39:01 -0300

    Created DetailsViewModel

--- FakeStore/App/Screens/2-Details/DetailsViewModel.swift
+++ FakeStore/App/Screens/2-Details/DetailsViewModel.swift
@@ -5,4 +5,17 @@
 //  Created by Diggo Silva on 21/07/25.
 //
 
-import Foundation
+import UIKit
+
+protocol DetailsViewModelProtocol {
+    var product: Product { get }
+}
+
+class DetailsViewModel: DetailsViewModelProtocol {
+    
+    let product: Product
+    
+    init(product: Product) {
+         self.product = product
+     }
+}
