4b4023c0dca7f0e753708d9757d93243bc2f4f6d
Diggo Silva <rdiggosilva@gmail.com>
2025-07-23 11:29:04 -0300

    Created SFSymbols file

--- FakeStore/App/Utils/SFSymbols.swift
+++ FakeStore/App/Utils/SFSymbols.swift
@@ -5,4 +5,10 @@
 //  Created by Diggo Silva on 22/07/25.
 //
 
-import Foundation
+import UIKit
+
+class SFSymbols {
+    static let star = UIImage(systemName: "star")?.withTintColor(.systemYellow, renderingMode: .alwaysOriginal)
+    static let starHalf = UIImage(systemName: "star.leadinghalf.filled")?.withTintColor(.systemYellow, renderingMode: .alwaysOriginal)
+    static let starFull = UIImage(systemName: "star.fill")?.withTintColor(.systemYellow, renderingMode: .alwaysOriginal)
+}
