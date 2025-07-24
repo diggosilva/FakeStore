58be0ab77d6b05a56c9545a556524cc8640d9d00
Diggo Silva <rdiggosilva@gmail.com>
2025-07-24 10:12:03 -0300

    Created CartManager file to add and remove items in the cart

--- FakeStore/App/Network/CartManager.swift
+++ FakeStore/App/Network/CartManager.swift
@@ -6,3 +6,48 @@
 //
 
 import Foundation
+import Combine
+
+class CartManager {
+    
+    static let shared = CartManager()
+    
+    private var cartItemsSubject = CurrentValueSubject<[Product], Never>([])
+    
+    var cartItemsPublisher: AnyPublisher<[Product], Never> {
+        return cartItemsSubject.eraseToAnyPublisher()
+    }
+    
+    var currentCartItems: [Product] {
+        return cartItemsSubject.value
+    }
+    
+    private init() {}
+    
+    func addProduct(product: Product) {
+        var currentItems = cartItemsSubject.value
+        currentItems.append(product)
+        cartItemsSubject.send(currentItems)
+        print("Produto adicionado ao carrinho: \(product.title). Total de itens: \(currentItems.count)")
+    }
+    
+    func removeProduct(product: Product) {
+        var currentItems = cartItemsSubject.value
+        if let index = currentItems.firstIndex(where: { $0.id == product.id }) {
+            currentItems.remove(at: index)
+            cartItemsSubject.send(currentItems)
+            print("Produto removido: \(product.title). Total de itens: \(currentItems.count)")
+        }
+    }
+    
+    func clearCart() {
+        cartItemsSubject.send([])
+        print("Carrinho limpo.")
+    }
+    
+    var cartTotal: Double {
+        return cartItemsSubject.value.reduce(0) { total, product in
+            total + product.price
+        }
+    }
+}
