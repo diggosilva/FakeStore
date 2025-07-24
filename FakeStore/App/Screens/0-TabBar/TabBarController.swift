e16df82993aab1373852ec3a048d20b00624eec8
Diggo Silva <rdiggosilva@gmail.com>
2025-07-24 11:49:45 -0300

    Created TabBar

--- FakeStore/App/Screens/0-TabBar/TabBarController.swift
+++ FakeStore/App/Screens/0-TabBar/TabBarController.swift
@@ -5,4 +5,46 @@
 //  Created by Diggo Silva on 24/07/25.
 //
 
-import Foundation
+import UIKit
+
+class TabBarController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configTabBar()
+    }
+    
+    private func configTabBar() {
+        let feedVC = UINavigationController(rootViewController: FeedViewController())
+        feedVC.tabBarItem.image = SFSymbols.house
+        feedVC.tabBarItem.selectedImage = SFSymbols.houseFill
+        feedVC.tabBarItem.title = "Produtos"
+        
+        let cartVC = UINavigationController(rootViewController: CartViewController())
+        cartVC.tabBarItem.image = SFSymbols.cart
+        cartVC.tabBarItem.selectedImage = SFSymbols.cartFill
+        cartVC.tabBarItem.title = "Carrinho"
+        
+        UITabBar.appearance().tintColor = .label
+        UITabBar.appearance().isTranslucent = false
+        
+        setViewControllers( [feedVC, cartVC], animated: true)
+    }
+}
+
+class CartViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureNavigationBar()
+    }
+    
+    private func configureNavigationBar() {
+        navigationItem.title = "Seu Carrinho"
+        view.backgroundColor = .yellow
+    }
+    
+    private func configureDelegatesAndDataSources() {
+        
+    }
+}
